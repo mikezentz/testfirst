d5cf5bd7cbd4f7f615cb9dce26e5054dbf94e790
josh <jhell85@gmail.com>
2019-09-05 13:37:14 -0700

    updated Books model

--- src/models/Books.js
+++ src/models/Books.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const userSchema = Schema({
-    Name: {
+const { ObjectId } = Schema.Types;
+const bookSchema = Schema({
+    title: {
         type: String,
         required: true,
+    },
+    available: {
+        type: Boolean,  
+    },
+    due_date: {
+        type: Date,
+    },
+    user: {
+        type: ObjectId,
+        ref: "User",
     }
 })
+const Book = mongoose.model("Book", bookSchema);
+module.export = Book
