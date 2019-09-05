db932b16f41ea27fc961811934b3abe2cb9021f8
josh <jhell85@gmail.com>
2019-09-05 15:57:05 -0700

    added shit

---
+++ src/routes/book.route.js
@@ -0,0 +1,12 @@
+const { AsyncRouter } = require("express-async-router");
+const Book = require("../models/Book");
+
+const router = AsyncRouter();
+
+// List
+router.get("/", async (req, res) => {
+    const books = await Book.find();
+    res.send(books);
+});
+
+//create
