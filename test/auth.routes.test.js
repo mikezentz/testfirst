eb6e1958a99910c7b6c30955a457f9b55fbbfb70
Mike Zentz <mzentz@gmail.com>
2019-09-04 18:19:51 -0700

    changes

--- test/auth.routes.test.js
+++ test/auth.routes.test.js
@@ -16,12 +16,15 @@ const invalidUser = {
 };
 
 describe("auth.routes.js", () => {
-  it("POST /auth/ should allow users to sign-up", async () => {
+  it("POST /auth/sign-up should allow users to sign-up", async () => {
     const response = await chai;
     request(app)
       .post("/auth/sign-up")
       .send(validUser);
 
     expect(response.status).to.equal(200);
+    expect(response.body.username).to.equal(validUser.username);
+    expect(response.body.password).to.equal(undefined);
   });
+  it("POST /auth/sign-up");
 });
