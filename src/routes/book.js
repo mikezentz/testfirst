4a3c77148def085604dec4c172dafba48a54b246
josh <jhell85@gmail.com>
2019-09-05 19:51:54 -0700

    updated book route

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,12 +1,18 @@
 const { AsyncRouter } = require("express-async-router");
+const { check, validationResult } = require("express-validator");
+
+const jwtMiddleware = require("../middleware/jwt");
 const Book = require("../models/Book");
 
 const router = AsyncRouter();
+const createValidators = [
+    check("name").exists(),
+    check("user").exists()
+];
 
 // List
-router.get("/", async (req, res) => {
+router.get("/list", async (req, res) => {
     const books = await Book.find();
     res.send(books);
 });
 
-//create
